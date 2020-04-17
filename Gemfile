d0f10cbe5b4e7c8ff9d89f98fc97ca7c70b4b2e5
Alex Lynch <mlynch5187@gmail.com>
2020-04-17 16:38:10 -0600

    Create merchant discounts index spec

--- Gemfile
+++ Gemfile
@@ -13,8 +13,11 @@ gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
 gem 'coffee-rails', '~> 4.2'
 gem 'jbuilder', '~> 2.5'
-gem 'bcrypt' 
+gem 'bcrypt'
 
+group :test do
+  gem "nyan-cat-formatter"
+end
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
---
+++ spec/features/merchants/discounts/index_spec.rb
@@ -0,0 +1,54 @@
+require 'rails_helper'
+
+RSpec.describe "As a merchant employee, when I visit discounts index" do
+  before(:each) do
+    @bike_shop = Merchant.create(name: "Brian's Bike Shop",
+                                 address: '123 Bike Rd.',
+                                 city: 'Richmond',
+                                 state: 'VA',
+                                 zip: 23137,
+                                 active: true)
+
+    @employee = User.create(name: "Mike Dao",
+               street_address: "1765 Larimer St",
+               city: "Denver",
+               state: "CO",
+               zip: "80202",
+               email: "mike@example.com",
+               password: "123456",
+               password_confirmation: "123456",
+               role: 1,
+               merchant_id: @bike_shop.id)
+
+    @tire = @bike_shop.items.create(name: "Bike Tire",
+                            description: "They'll never pop!",
+                            price: 200,
+                            image: "https://mk0completetrid5cejy.kinstacdn.com/wp-content/uploads/2013-Shimano-Wheels-WH9000-C50-tubular-clincher.jpg",
+                            inventory: 10)
+    @helmet = @bike_shop.items.create(name: "Bike Helmet",
+                            description: "They'll never crack!",
+                            price: 100,
+                            image: "https://cdn.shopify.com/s/files/1/0836/6919/products/vintage_bike_helmet_001_2000x.jpg?v=1585087482",
+                            inventory: 12)
+
+    @discount_1 = @bike_shop.discounts.create(name: "Buy Three, Get 10% Off",
+                                              percentage: 0.1)
+
+    @discount_2 = @bike_shop.discounts.create(name: "Buy Three, Get 10% Off",
+                                              percentage: 0.1)
+
+    visit "/login"
+
+    fill_in :email, with: @employee.email
+    fill_in :password, with: "123456"
+
+    click_button "Login"
+    visit "/merchant"
+  end
+
+  it "shows all discount names as links to discount showpage" do
+
+      expect(page).to have_link("#{@discount_1.name}")
+      expect(page).to have_link("#{@discount_2.name}")
+  end
+end
