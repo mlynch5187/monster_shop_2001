cfd290dad8548117722c2c7379df8005402634b0
Alex Lynch <mlynch5187@gmail.com>
2020-04-18 08:53:19 -0600

    Complete discount showpage

--- spec/features/merchants/discounts/show_spec.rb
+++ spec/features/merchants/discounts/show_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe "As a merchant employee, when I visit discounts index" do
+RSpec.describe "As a merchant employee, when I visit a discount's showpage" do
   before(:each) do
     @bike_shop = Merchant.create(name: "Brian's Bike Shop",
                                  address: '123 Bike Rd.',
@@ -18,7 +18,18 @@ RSpec.describe "As a merchant employee, when I visit discounts index" do
                password: "123456",
                password_confirmation: "123456",
                role: 1,
-               merchant_id: @bike_shop.id)    
+               merchant_id: @bike_shop.id)
+
+    @tire = @bike_shop.items.create(name: "Bike Tire",
+                            description: "They'll never pop!",
+                            price: 200,
+                            image: "https://mk0completetrid5cejy.kinstacdn.com/wp-content/uploads/2013-Shimano-Wheels-WH9000-C50-tubular-clincher.jpg",
+                            inventory: 10)
+    @helmet = @bike_shop.items.create(name: "Bike Helmet",
+                            description: "They'll never crack!",
+                            price: 100,
+                            image: "https://cdn.shopify.com/s/files/1/0836/6919/products/vintage_bike_helmet_001_2000x.jpg?v=1585087482",
+                            inventory: 12)
 
     @discount_1 = @bike_shop.discounts.create(name: "Buy Three, Get 10% Off",
                                               percentage: 0.1,
@@ -37,16 +48,20 @@ RSpec.describe "As a merchant employee, when I visit discounts index" do
 
     click_button "Login"
     visit "/merchant"
+
+    within("#discount-#{@discount_1.id}") do
+      click_link "#{@discount_1.name}"
+    end
   end
 
-  it "shows all discount names as links to discount showpage" do
+  it "shows a discount's name, percentage off, number of items required, and description" do
 
-      expect(page).to have_link("#{@discount_1.name}")
-      expect(page).to have_link("#{@discount_2.name}")
+    save_and_open_page
 
-      within("#discount-#{@discount_1.id}") do
-        click_link "#{@discount_1.name}"
-      end
+      expect(page).to have_content("#{@discount_1.name}")
+      expect(page).to have_content("#{@discount_1.percentage.to_i}")
+      expect(page).to have_content("#{@discount_1.bulk}")
+      expect(page).to have_content("#{@discount_1.description}")
 
       expect(current_path).to eq("/merchant/discounts/#{@discount_1.id}")
   end
